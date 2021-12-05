30dbc2677d5f75d67d33d144a194867737be2c2a
Alessandro Ferro <49845537+xfarrow@users.noreply.github.com>
2021-12-05 14:33:57 +0100

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,32 +1,33 @@
 ; Boot text (Unina-OS)
-mov ah, 0x0e
+mov ah, 0x0e ; 0x0e must be in the ah in order to be able to print
+
 mov al, 'U'
 int 0x10 ; interrupt which triggers an output on screen of the value stored in al register
-mov ah, 0x0e
+
 mov al, 'n'
 int 0x10
-mov ah, 0x0e
+
 mov al, 'i'
 int 0x10
-mov ah, 0x0e
+
 mov al, 'n'
 int 0x10
-mov ah, 0x0e
+
 mov al, 'a'
 int 0x10
-mov ah, 0x0e
+
 mov al, '-'
 int 0x10
-mov ah, 0x0e
+
 mov al, 'O'
 int 0x10
-mov ah, 0x0e
+
 mov al, 'S'
 int 0x10
-mov ah, 0x0e
+
 mov al, 10 ; new line
 int 0x10
-mov ah, 0x0e
+
 mov al, 13 ; carriage return
 int 0x10
 
@@ -41,6 +42,20 @@ Print_description:
   inc bx ; add 1 to the value of the pointer bx so it points to the next character
   jmp Print_description
 
+Description_text:
+  db "The only thing that this OS does is printing the alphabet. Wow."
+  db 10 ; new line
+  db 13 ; carriage return
+  db 0  ; end of string
+
+;
+; Data and code are the same thing, so Description_text code block just places
+; (thanks to the db instruction) arbitrary data in memory. Print_description
+; will read this data byte by byte.
+; You can view this by imaging the memory like an array where the entire program
+; gets loaded and some parts are data and others are code.
+;
+
 exit_description:
 
 ; prints the alphabet
@@ -54,13 +69,7 @@ print_alphabet:
   jmp print_alphabet
 exit_printing_alphabet:
 
-Description_text:
-  db "The only thing that this OS does is printing the alphabet. Wow."
-  db 10 ; new line
-  db 13 ; carriage return
-  db 0  ; end of string
-
 ; bootable sector
-jmp $
+jmp $ ; useless, jumps on the next instruction
 times 510-($-$$) db 0
 db 0x55, 0xaa
